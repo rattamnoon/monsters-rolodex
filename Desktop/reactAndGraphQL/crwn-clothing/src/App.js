8f194b6727aa6defd76e57131df818e535db4f34
rattamnoon <rattamnoon.kir@gmail.com>
2020-11-12 18:11:22 +0700

    adding header component antd links to home , shop page and contact

--- Desktop/reactAndGraphQL/crwn-clothing/src/App.js
+++ Desktop/reactAndGraphQL/crwn-clothing/src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './page/homepage/homepage.component';
 import ShopPage from './page/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header /> 
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
