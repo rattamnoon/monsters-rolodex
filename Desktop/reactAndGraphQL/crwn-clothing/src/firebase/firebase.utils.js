efe142612853585a178b7c9221d5c3815244e456
rattamnoon <rattamnoon.kir@gmail.com>
2020-11-13 16:49:33 +0700

    seccess edit name

--- Desktop/reactAndGraphQL/crwn-clothing/src/firebase/firebase.utils.js
+++ Desktop/reactAndGraphQL/crwn-clothing/src/firebase/firebase.utils.js
@@ -13,7 +13,34 @@ const config = {
     measurementId: "G-B4R3DSPW6C"
 };
 
-firebase.initializeApp(config);
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
