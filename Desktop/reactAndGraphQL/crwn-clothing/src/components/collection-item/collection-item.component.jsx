5378e702e6d98bd4aabd02d290641280a9f6c6c5
rattamnoon <rattamnoon.kir@gmail.com>
2020-11-14 08:54:59 +0700

    updated shop data with new data, updated cart utils to group item in cart reducer

--- Desktop/reactAndGraphQL/crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ Desktop/reactAndGraphQL/crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
 
-const CollectioItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item" key={id}>
-    <div
-      className="image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+const CollectioItem = ({ item, addItem }) => {
+  const { imageUrl, name, price } = item;
+  return (
+    <div className="collection-item">
+      <div
+        className="image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        {" "}
+        Add to cart{" "}
+      </CustomButton>
     </div>
-  </div>
-);
+  );
+};
 
-export default CollectioItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectioItem);
--- Desktop/reactAndGraphQL/crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
+++ Desktop/reactAndGraphQL/crwn-clothing/src/components/collection-preview/collection-preview.component.jsx
@@ -9,8 +9,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-          <CollectioItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollectioItem key={item.id} item={item} />
         ))}
     </div>
   </div>
