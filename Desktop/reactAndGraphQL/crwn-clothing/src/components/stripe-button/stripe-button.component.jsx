b0e67701ea42ba11a663d951d6576a2e4d5a0a47
rattamnoon <rattamnoon.kir@gmail.com>
2020-11-21 17:12:23 +0700

    creat loading page

--- Desktop/reactAndGraphQL/crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
+++ Desktop/reactAndGraphQL/crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -1,28 +1,25 @@
-import React from "react";
-import StripeCheckout from "react-stripe-checkout";
-
-import "./stripe-button.styles.scss";
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey =
-    "pk_test_51HoKhYJdr66zpDwwZhFfM4pHmzNEItlFIsANrygUyQK2UTRWQwJfcXEksIEXBn77b6YYaNU94OHhHF3ZDf1M3sEU00h28Y4N9u";
+  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
 
-  const onToken = (token) => {
+  const onToken = token => {
     console.log(token);
-    alert("Payment Successful");
+    alert('Payment Succesful!');
   };
 
   return (
     <StripeCheckout
-      label="Pay Now"
-      name="CRWN Clothing Ltd."
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/up/d/f3eb2117da"
-      description={`Your total is ${price}`}
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel="Pay Now"
+      panelLabel='Pay Now'
       token={onToken}
       stripeKey={publishableKey}
     />
